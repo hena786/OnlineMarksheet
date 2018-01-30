7ec24813f39cb663a3eebe2b44c80fbcf3561edd
hena <hena@gmail.com>
2018-01-30 13:05:47 +0530

    Add

--- login.php
+++ login.php
@@ -15,7 +15,31 @@
                 -moz-background-size: cover;
                 -o-background-size: cover;
                 background-size: cover;">
-        
+        <div class="container" style="background-image: url('backgroung.jpg');">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-4">
+                    <form id="LoginForm">
+                        <div class="form-group">
+                            <div class="col-sm-4">
+                            <label for="inputId">Login Id</label>
+                            </div>
+                            <div class="col-sm-12">
+                            <input type="text" class="form-control" id='inputId'>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-4">
+                            <label for="inputPass">Password</label>
+                            </div>
+                            <div class="col-sm-12">
+                            <input type="text" class="form-control" id='inputPass'>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
         
     </body>
 </html>
