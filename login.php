89dc492253d2fadc926ad5d93cefa8bdfdf5fb8e
hena <hena@gmail.com>
2018-01-30 12:14:42 +0530

    Added Background Image

---
+++ login.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Login </title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body style="background-image: url('img/body.jpg'); 
+          -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;">
+        
+        
+    </body>
+</html>
