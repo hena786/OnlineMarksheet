a2a4eae5fc70b485643047a625242d3260cc1ca5
hena <hena@gmail.com>
2018-01-30 16:59:19 +0530

    Invalid Credential

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
 
         <!-- Latest compiled JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css" type="text/css">
     </head>
     <body style="background-image: url('img/body.jpg'); 
           -webkit-background-size: cover;
@@ -21,30 +21,41 @@
             <div class="row" >
                 <div class="col-sm-4 col-sm-offset-4 jumbotron" >
                     <img src="img/login.gif" height="50px" width="200px">
-                    <form id="LoginForm">
+                    <form class="form-horizontal" id="userLogin">
                         
                         <div class="form-group">
                             <div class="col-sm-4">
-                            <label for="inputId">Login Id</label>
+                            <label for="inputId" class="control-label" >Login Id</label>
                             </div>
                             <div class="col-sm-12">
-                            <input type="text" class="form-control" id='inputId'>
+                                <input type="text" class="form-control" id='inputId'>
                             </div>
+                        </div>
+                        <div id="validId">
                         </div>
                         <div class="form-group">
                             <div class="col-sm-4">
-                            <label for="inputPass">Password</label>
+                            <label for="inputPass" class="control-label">Password</label>
                             </div>
                             <div class="col-sm-12">
                             <input type="password" class="form-control" id='inputPass'>
                             </div>
                         </div>
+                        <div id="validPass">
+                        </div>
                         <div class="form-group">
                             <div class="col-sm-12">
+                            <input type="checkbox" name="rememberme" value="Remember Me">
+                            <span><label for="remember">Remember Me</label></span>
                             </div>
+                        </div>
+                        <div class="form-group">
+                            
                             <div class="col-sm-12">
-                                <input type="submit" class="btn btn-success btn-lg" id="Submit" style="border-bottom: 3px solid #000; border-radius: 50px;">
+                                <input type="submit" class="btn btn-danger btn-lg" id="Submit" style="border-bottom: 8px solid #000; border-radius: 50px;" value="Log In">
                             </div>
+                        </div>
+                        <div id="signIn">
                         </div>
                         
                         
@@ -52,6 +63,34 @@
                 </div>
             </div>
         </div>
-        
+        <script>
+            $("#userLogin").submit(function(e){
+                e.preventDefault();
+                $('#signIn').html('');
+                var id = $('#inputId').val();
+                var password = $('#inputPass').val();
+                if(id === '' || password === ''){
+                    $('#signIn').html('<p>Invalid Credential</p>');
+                }else
+                {
+                     var dataString = "id="+id+"&password="+password;
+                     // AJAX Code To Submit Form.
+                    $.ajax({
+                        type: "POST",
+                        url: "module/login_mod.php",
+                        data: dataString,
+                        cache: false,
+                        success: function(result){
+                            if(result === 'success'){
+                                window.location.replace('home.php');
+                            }else{
+                                $('#signIn').html('<p>'+result+'</p>');
+                            }
+                        }
+                    });
+                }
+            });
+            
+        </script>
     </body>
 </html>
